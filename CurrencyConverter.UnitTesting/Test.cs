63188fb8b86aeea8aae1f131fc2a742f56c4139c
lcouturier <lcouturier75@free.fr>
2017-11-22 10:02:24 +0100

    Correction des Tests Unitaires

--- CurrencyConverter.UnitTesting/Test.cs
+++ CurrencyConverter.UnitTesting/Test.cs
@@ -1,34 +1,30 @@
-﻿using NUnit.Framework;
-
-
-namespace CurrencyConverter.UnitTesting
+﻿namespace CurrencyConverter.UnitTesting
 {
-    using LibraryNET;
+	using System.Diagnostics;
+	using Library.Core;
+	using Library;
+	using NUnit.Framework;
 
-    using System.Diagnostics;
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestLoadRates()
+		{
+			var items = RatesFactory.LoadAsync().Result;
+			Assert.IsNotNull(items);
 
-    using LibraryNET.Core;
-
-    [TestFixture()]
-    public class Test
-    {
-        [Test()]
-        public void TestLoadRates()
-        {
-            var items = RatesFactory.LoadAsync().Result;
-            Assert.IsNotNull(items);
-
-            Trace.WriteLine(items.ToString());
-        }
+			Trace.WriteLine(items.ToString());
+		}
 
 
-        [Test()]
-        public void TestJson()
-        {
-            const string Value = "{ \"base\":\"EUR\",\"date\":\"2017-11-20\",\"rates\":{ \"AUD\":1.5592,\"BGN\":1.9558,\"BRL\":3.8388} }";
-            var result = StringExtensions.DeserializeJson<Rates>(Value);
+		[Test()]
+		public void TestJson()
+		{
+			const string Value = "{ \"base\":\"EUR\",\"date\":\"2017-11-20\",\"rates\":{ \"AUD\":1.5592,\"BGN\":1.9558,\"BRL\":3.8388} }";
+			var result = StringExtensions.DeserializeJson<Rates>(Value);
 
-            Trace.WriteLine(result.ToString());
-        }
-    }
+			Trace.WriteLine(result.ToString());
+		}
+	}
 }
