3a90f845ba3f13db867af297ec0860785b7b8ac3
lcouturier <lcouturier75@free.fr>
2017-11-22 09:18:02 +0100

    * LibraryNET.csproj: Création de la classe RatesFactory
    * Rates.cs:
    * RatesFactory.cs:

--- LibraryNET/Rates.cs
+++ LibraryNET/Rates.cs
@@ -1,36 +1,9 @@
 ﻿
 namespace LibraryNET
-{
-    using System.IO;
-    using System.Net.Http;
+{    
     using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Threading.Tasks;
     using Newtonsoft.Json;    
-
-    public static class RatesFactory
-    {
-        /// <summary>
-        /// Loads the asynchronous.
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="FileNotFoundException">Le fichier est introuvable.</exception>
-        /// <exception cref="DirectoryNotFoundException">Le chemin d'accès spécifié n'est pas valide, il se trouve par exemple sur un lecteur non mappé.</exception>
-        /// <exception cref="IOException"><paramref name="path" /> comprend une syntaxe incorrecte ou non valide pour les noms de fichiers, les noms de répertoires ou les noms de volumes.</exception>
-        public static async Task<Rates> LoadAsync()
-        {
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync("https://api.fixer.io/latest");
-                var result = response.Content.ReadAsStringAsync().Result;
-                Trace.WriteLine(result);
-
-                return result.DeserializeJson<Rates>();
-            }
-        }
-    }
-
-
+   
 
     public class Rates
 	{	
---
+++ LibraryNET/RatesFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LibraryNET
+{
+	public static class RatesFactory
+	{
+		/// <summary>
+		/// Loads the asynchronous.
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="FileNotFoundException">Le fichier est introuvable.</exception>
+		/// <exception cref="DirectoryNotFoundException">Le chemin d'accès spécifié n'est pas valide, il se trouve par exemple sur un lecteur non mappé.</exception>
+		/// <exception cref="IOException"><paramref name="path" /> comprend une syntaxe incorrecte ou non valide pour les noms de fichiers, les noms de répertoires ou les noms de volumes.</exception>
+		public static async Task<Rates> LoadAsync()
+		{
+			using (var client = new HttpClient())
+			{
+				var response = await client.GetAsync("https://api.fixer.io/latest");
+				var result = response.Content.ReadAsStringAsync().Result;
+				Trace.WriteLine(result);
+
+				return result.DeserializeJson<Rates>();
+			}
+		}	 }
+}
